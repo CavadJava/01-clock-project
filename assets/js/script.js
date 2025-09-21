185ef236710a73e1635e14e9933728e97d70b8e0
javad <j.bagirov@expressbank.az>
2025-09-21 16:51:15 -0700

    timeout and colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,49 @@ const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 
 function updateClock() {
-    console.log('Updating clock...')
     const now = new Date();
-    console.log(now);
     const h = now.getHours().toString().padStart(2, '0');
-    console.log(h);
     const m = now.getMinutes().toString().padStart(2, '0');
     const s = now.getSeconds().toString().padStart(2, '0');
 
-    minute.style.color = m%2==0 ? 'green'  : 'blue';
-    second.style.color =s%2==0 ? 'green'  : 'red';
-    hour.style.color = h%2==0 ? 'green'  : 'red';
+    minute.style.color = m%2==0 ? randomColor()  : randomColor();
+    second.style.color =s%2==0 ? randomColor()  : randomColor();
+    hour.style.color = h%2==0 ? randomColor()  : randomColor();
 
     hour.textContent = h;
     minute.textContent = m;
     second.textContent = s;
 }
-let updateCLockVar = setInterval(updateClock, 1000);
 
+let randomColor = function randomColor() {
+    let colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple'];
+
+    let randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+let updateCLockVar = setInterval(updateClock, 1000);
+function callUpdateClock(){
+    return updateCLockVar();
+}
 document.querySelector(".startBtn").addEventListener("click", ()=>{
     updateCLockVar = setInterval(updateClock, 1000);
 });
 document.querySelector(".stopBtn").addEventListener("click", () => {
     clearInterval(updateCLockVar);
+    updateCLockVar = null;
 })
+
+document.querySelector(".timerBtn").addEventListener("click", () => {
+    let timer = prompt("Enter the time in seconds");
+    document.querySelector(".timerBtn").disabled = true;
+    clearInterval(updateCLockVar);
+    updateCLockVar = null;
+    document.querySelector("#timer").textContent = `${timer}`;
+        document.querySelector(".timerBtn").textContent = 'Triggered';
+    setTimeout(() => {
+        document.querySelector("#timer").textContent = 'done';
+        document.querySelector(".timerBtn").disabled = false;
+        updateCLockVar = setInterval(updateClock, 1000);
+    }, timer * 1000);
+});
